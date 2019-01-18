f598439caac9aa7326f35eca9103378ea2cfe3d1
PESURF <rohithrajreganti@gmail.com>
2019-01-18 14:22:33 +0530

    db selection running

--- dbhandler.py
+++ dbhandler.py
@@ -4,7 +4,7 @@ def get_data_from_database():
   
 
     conn = sqlite3.connect('test.db')
-
+    """
     conn.execute('''CREATE TABLE COMPANY
             (ID INT PRIMARY KEY     NOT NULL,
             NAME           TEXT    NOT NULL,
@@ -12,5 +12,29 @@ def get_data_from_database():
             ADDRESS        CHAR(50),
             SALARY         REAL);''')
     print "Table created successfully"
+    
 
+    conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )")
+
+    conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
+
+    conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+
+    conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+
+    conn.commit()
+    """
+
+    cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+    ll=[]
+    for row in cursor:
+        ll.append(row)
+
+
+  
     conn.close()
+    return ll
