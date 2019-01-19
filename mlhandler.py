63fc0a30a460d9ec3b24f1b8e2dfe64fa55b2fc3
PESURF <rohithrajreganti@gmail.com>
2019-01-19 13:54:06 +0530

    saving fig in matplot lib

--- mlhandler.py
+++ mlhandler.py
@@ -2,8 +2,20 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import mpld3 #used to render matplotlib output in webpages
 
 
 def get_data_from_mlscript():
-    a = np.array([2,3,4])
-    return  a
+    fig, ax = plt.subplots(1, 1)
+    ax.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
+    fig.savefig('static/images/matplot_img.png')
+
+    """
+    #save fig to a html file
+    fig, ax = plt.subplots(1, 1)
+    ax.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
+    with open('templates/plot_fig.html','w') as fl:
+        mpld3.save_html(fig,fl)
+
+    """
+        
