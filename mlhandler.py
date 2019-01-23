8dc79241317f9c1b0b7a611a3a501c3ab2ecdd59
PESURF <rohithrajreganti@gmail.com>
2019-01-23 10:54:47 +0530

    mlhandler.py changed

--- mlhandler.py
+++ mlhandler.py
@@ -9,11 +9,11 @@ import os
 def get_data_from_mlscript():
     #t = np.arange(0.0, 2.0, 0.01)
     #s = 1 + np.sin(2 * np.random.randint(0,100) * t)
-    x = np.random.uniform(-3, 3, size=20)
-    y = np.random.uniform(-3, 3, size=20)
+    x = np.random.uniform(-3, 3, size=50)
+    y = np.random.uniform(-3, 3, size=50)
     plt.gcf().clear()
    # plt.plot(t,s)
-    plt.scatter(x,y)
+    plt.scatter(x,y,s=70,c='red')
     try:
         os.remove('static/images/new_plot.png')
     except:
